c551ff38cca4a54c9f7fda7d64e799caa8d64c61
dima <dimvalyaev@gmai.com>
2023-12-08 21:48:57 +0500

    delete isNew

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -50,10 +50,6 @@ public class Meal extends AbstractBaseEntity{
         return dateTime.toLocalTime();
     }
 
-    public boolean isNew() {
-        return id == null;
-    }
-
     @Override
     public String toString() {
         return "Meal{" +
