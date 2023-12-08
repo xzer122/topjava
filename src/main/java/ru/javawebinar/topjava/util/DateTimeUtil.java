4c289e7ff98124bad91b3f2f11fa43b0e9086b14
dima <dimvalyaev@gmai.com>
2023-12-08 22:00:36 +0500

    rename lt to local

--- src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
@@ -6,8 +6,8 @@ import java.time.format.DateTimeFormatter;
 public class DateTimeUtil {
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
-    public static <T extends Comparable<T>> boolean isBetweenHalfOpen(T lt, T startTime, T endTime) {
-        return lt.compareTo(startTime) >= 0 && lt.compareTo(endTime) <= 0;
+    public static <T extends Comparable<T>> boolean isBetweenHalfOpen(T local, T startTime, T endTime) {
+        return local.compareTo(startTime) >= 0 && local.compareTo(endTime) <= 0;
     }
 
     public static String toString(LocalDateTime ldt) {
