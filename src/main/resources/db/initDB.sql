7c838ff806ea8fd88bc8bf5830a1a0bc06676829
dima <dimvalyaev@gmai.com>
2024-01-23 19:28:21 +0500

    good index fix

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -28,10 +28,10 @@ CREATE TABLE user_role
 CREATE TABLE meals
 (
     id          INTEGER     PRIMARY KEY DEFAULT nextval('global_seq'),
-    date_time   TIMESTAMP               DEFAULT now()  NOT NULL,
+    date_time   TIMESTAMP                              NOT NULL,
     description VARCHAR                                NOT NULL,
     calories    INTEGER                                NOT NULL,
     user_id     INTEGER                                NOT NULL,
-    CONSTRAINT meal_idx UNIQUE (date_time, user_id),
+    CONSTRAINT meal_userid_dt_idx_unique UNIQUE (user_id, date_time),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 )
